18d9ac53bf460b24bb56c00cf5765c413c87c55c
LAPTOP-GGEN23LJ\Tania <tania.babylonradio@gmail.com>
2017-06-30 16:05:56 +0100

    sideleft menu changes

---
+++ routes/views/culture.js
@@ -0,0 +1,14 @@
+var keystone = require('keystone');
+
+exports = module.exports = function(req, res) {
+
+	var view = new keystone.View(req, res),
+		locals = res.locals;
+
+	locals.section = 'culture';
+	locals.page.title = 'Culture';
+
+
+	view.render('site/culture');
+
+}
