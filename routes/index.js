ef6a9808be6129bc8d025abc45ec57275724544e
carlos <bobcn_1991@hotmail.com>
2017-07-01 02:56:32 +0100

    'Links' section and Comments working
    localhost:3000/links
    
    Links can be re-used for Classifieds or Articles etc, same as the Blog.

--- routes/index.js
+++ routes/index.js
@@ -84,9 +84,9 @@ exports = module.exports = function (app) {
 	//app.get('/members/mentors', routes.views.mentors);
 	app.get('/member/:member', routes.views.member);
 	//app.get('/organisations', routes.views.organisations);
-	//app.get('/links', routes.views.links);
-	//app.get('/links/:tag?', routes.views.links);
-	//app.all('/links/link/:link', routes.views.link);
+	app.get('/links', routes.views.links);
+	app.get('/links/:tag?', routes.views.links);
+	app.all('/links/link/:link', routes.views.link);
 	app.get('/blog/:category?', routes.views.blog);
 	app.all('/blog/post/:post', routes.views.post);
 	app.get('/about', routes.views.about);
